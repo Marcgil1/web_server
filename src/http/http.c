4a8fbe37522c62be03fecd761e174c92f5ccdc45
Marcgil1 <marcos.gile@protonmail.com>
2021-03-22 23:52:20 +0000

    Implemented and tested http_read_request

--- src/http/http.c
+++ src/http/http.c
@@ -10,7 +10,7 @@
 #define BUFSIZE 8096
 
 int __read_request(char* buffer, http_req_t* msg, http_error_t* error);
-int __read_headers(char* buffer, http_header_t* headers, unsigned* num_headers, http_error_t* error); 
+int __read_headers(char* buffer, http_header_t** headers, unsigned* num_headers, http_error_t* error); 
 int __read_body(char* buffer, http_req_t* msg, http_error_t* error); 
 
 http_req_t* http_read_request(int fd, http_error_t* error) {
@@ -18,6 +18,7 @@ http_req_t* http_read_request(int fd, http_error_t* error) {
     uint8_t buffer[BUFSIZE + 1] = { 0 };
     unsigned readsize = 0;
     unsigned bytes_read = 0;
+    int start;
     int ret;
 
     msg = malloc(sizeof(http_req_t));
@@ -36,20 +37,24 @@ http_req_t* http_read_request(int fd, http_error_t* error) {
         return NULL;
     }
 
+    start = 0;
     ret = __read_request(buffer, msg, error);
-    if (ret) {
+    if (ret < 0) {
         http_drop_request(msg);
         return NULL;
     }
 
-    ret = __read_headers(buffer, msg->headers, &(msg->num_headers), error);
-    if (ret) {
+    start += ret;
+    ret = __read_headers(buffer + start, &(msg->headers), &(msg->num_headers), error);
+    if (ret < 0) {
+        printf("!!! (%s) %d\n", buffer + start, ret);
         http_drop_request(msg);
         return NULL;
     }
 
-    ret = __read_body(buffer, msg, error);
-    if (ret) {
+    start += ret;
+    ret = __read_body(buffer + start, msg, error);
+    if (ret < 0) {
         http_drop_request(msg);
         return NULL;
     }
@@ -76,24 +81,26 @@ int __read_request(char* buffer, http_req_t* msg, http_error_t* error) {
     unsigned end_idx;
     unsigned len;
     unsigned start;
+    unsigned num_groups;
     int regi;
 
+    num_groups = 5;
     regi = regcomp(
         &reg_request,
-        "(GET|POST|HEAD|PUT|DELETE) ([[:alpha:]]+.[[:alpha:]]+.[[:alpha:]]+) (HTTP/[0-9].[0-9])\r\n",
+        "(GET|POST|HEAD|PUT|DELETE) (/|(/([[:alpha:]]|\.)+)+) (HTTP/[0-9]\.[0-9])\r\n",
         REG_EXTENDED);
     if (regi) {
         // Should never get here.
     }
 
-    pmatch = malloc(4 * sizeof(regmatch_t));
+    pmatch = malloc((num_groups+1) * sizeof(regmatch_t));
     if (pmatch == NULL) {
         *error = SYSTEM_ERROR;
         regfree(&reg_request);
         return -1;
     }
     
-    regi = regexec(&reg_request, buffer, 4, pmatch, 0);
+    regi = regexec(&reg_request, buffer, num_groups+1, pmatch, 0);
     if (regi || pmatch[0].rm_so != 0) {
         *error = CORRUPTED_MESSAGE;
         regfree(&reg_request);
@@ -141,15 +148,17 @@ int __read_request(char* buffer, http_req_t* msg, http_error_t* error) {
         return -3;
     }
     strncpy(msg->url, buffer + start, len);
+    msg->url[len] = '\0';
 
-    len = pmatch[3].rm_eo - pmatch[3].rm_so;
-    start = pmatch[3].rm_so;
+    len = pmatch[5].rm_eo - pmatch[5].rm_so;
+    start = pmatch[5].rm_so;
     msg->version = malloc((len + 1) * sizeof(char));
     if (msg->version == NULL) {
         *error = SYSTEM_ERROR;
         return -4;
     }
     strncpy(msg->version, buffer + start, len);
+    msg->version[len] = '\0';
 
     return start + len + 2;
 }
@@ -157,28 +166,96 @@ int __read_request(char* buffer, http_req_t* msg, http_error_t* error) {
 /* Checks how many lines in the buffer conform to the header line specification.
  * parse them and set `headers` and `num_headers` accordingly.
  */
-int __read_headers(char* buffer, http_header_t* headers, unsigned* num_headers, http_error_t* error) {
+int __read_headers(char* buffer, http_header_t** headers, unsigned* num_headers, http_error_t* err) {
     regex_t reg_header;
-    regmatch_t* pmatch[MAX_HEADERS];
+    unsigned num_groups = 4;
+    regmatch_t pmatch[num_groups+1];
+    unsigned top;
+    unsigned idx;
+    unsigned len;
+    unsigned start;
     int regi;
+
+    *headers = malloc(MAX_HEADERS * sizeof(http_header_t));
+    if (*headers == NULL) {
+        *err = SYSTEM_ERROR;
+        return -1;
+    }
 
     regi = regcomp(
         &reg_header,
-        "(([[:alnum:]])+: ([[:alnum:]])+\r\n)*\r\n",
+        "^(([[:alnum:]]|-|_)+): (([[:alnum:]]|-|_|[[:punct:]])+)\r\n",
         REG_EXTENDED);
     if (regi) {
         // Should never get here.
     }
-    
 
-    return 1;
+    idx = 0;
+    top = 0;
+    while ((regi = regexec(&reg_header, buffer + top, num_groups+1, pmatch, 0))
+           != REG_NOMATCH)
+    {
+        len = pmatch[1].rm_eo - pmatch[1].rm_so;
+        start = pmatch[1].rm_so;
+        (*headers)[idx].field_name = malloc((len + 1) * sizeof(char));
+        // Must delete allocated memory!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if ((*headers)[idx].field_name == NULL) {
+            *err = SYSTEM_ERROR;
+            return -2;
+        }
+        strncpy((*headers)[idx].field_name, buffer + top + start, len);
+        (*headers)[idx].field_name[len] = '\0';
+        
+        len = pmatch[3].rm_eo - pmatch[3].rm_so;
+        start = pmatch[3].rm_so;
+        (*headers)[idx].value = malloc((len + 1) * sizeof(char));
+        if ((*headers)[idx].value == NULL) {
+            *err = SYSTEM_ERROR;
+            return -3;
+        }
+        strncpy((*headers)[idx].value, buffer + top + start, len);
+        (*headers)[idx].value[len] = '\0';
+
+        idx++;
+        top += pmatch[0].rm_eo;
+        
+        
+        if (idx == MAX_HEADERS) {
+            *err = CORRUPTED_MESSAGE;
+            return -4;
+        }
+    }
+ 
+    *num_headers = idx;
+    if (strncmp(buffer + top, "\r\n", 2) != 0) {
+        *err = CORRUPTED_MESSAGE;
+        return -5;
+    }
+
+    if (realloc(*headers, *num_headers * sizeof(http_header_t)) == NULL) {
+        *err = SYSTEM_ERROR;
+        return -6;
+    }
+
+    return top + 2;
 }
 
 /*
  * Searches for the body of an http reques message in `buffer` and stores a
  * copy of it in `msg`.
  */
-int __read_body(char* buffer, http_req_t* msg, http_error_t* error) {
+int __read_body(char* buffer, http_req_t* msg, http_error_t* err) {
+    int len;
+    
+    len = strlen(buffer);
+
+    msg->body = malloc(len * sizeof(char));
+    if (msg->body == NULL) {
+        *err = SYSTEM_ERROR;
+        return -1;
+    }
+
+    strcpy(msg->body, buffer);
 }
 
 http_res_t* http_read_response(int fd, http_error_t* error) {
--- src/http/http.test.c
+++ src/http/http.test.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 void setUp() {
     // Nothing.
@@ -13,51 +14,51 @@ void tearDown() {
     // Nothing.
 }
 
-void test_can_read_request1() {
+void test_can_read_request_with_empty_url() {
     int ret;
     http_req_t msg;
     http_error_t err;
 
     ret = __read_request(
-        "GET website.h.com HTTP/1.1\r\n"
+        "GET / HTTP/1.1\r\n"
         "status: val\r\n"
         "\r\n"
         "body body body\r\n"
         "\r\n",
         &msg,
         &err);
-    TEST_ASSERT_EQUAL_INT(28,                   ret);
-    TEST_ASSERT_EQUAL_INT(GET,                  msg.method);
-    TEST_ASSERT_EQUAL_STRING("website.h.com",   msg.url);
-    TEST_ASSERT_EQUAL_STRING("HTTP/1.1",        msg.version);
+    TEST_ASSERT_EQUAL_INT(16,            ret);
+    TEST_ASSERT_EQUAL_INT(GET,           msg.method);
+    TEST_ASSERT_EQUAL_STRING("/",        msg.url);
+    TEST_ASSERT_EQUAL_STRING("HTTP/1.1", msg.version);
 }
 
-void test_can_read_request2() {
+void test_can_read_request_with_file_in_url() {
     int ret;
     http_req_t msg;
     http_error_t err;
 
     ret = __read_request(
-        "POST website.hff.com HTTP/1.4\r\n"
+        "POST /index.html HTTP/1.4\r\n"
         "status: val\r\n"
         "\r\n"
         "body body body\r\n"
         "\r\n",
         &msg,
        &err);
-    TEST_ASSERT_EQUAL_INT(31,                   ret);
-    TEST_ASSERT_EQUAL_INT(POST,                 msg.method);
-    TEST_ASSERT_EQUAL_STRING("website.hff.com", msg.url);
-    TEST_ASSERT_EQUAL_STRING("HTTP/1.4",        msg.version);
+    TEST_ASSERT_EQUAL_INT(27,               ret);
+    TEST_ASSERT_EQUAL_INT(POST,             msg.method);
+    TEST_ASSERT_EQUAL_STRING("/index.html", msg.url);
+    TEST_ASSERT_EQUAL_STRING("HTTP/1.4",    msg.version);
 }
 
-void test_can_read_request3() {
+void test_fails_to_read_ill_formed_request() {
     int ret;
     http_req_t msg;
     http_error_t err;
 
     ret = __read_request(
-        "PASD website.hff.com HTTP/1.4\r\n"
+        "PASD /index.html HTTP/1.4\r\n"
         "status: val\r\n"
         "\r\n"
         "body body body\r\n"
@@ -68,15 +69,144 @@ void test_can_read_request3() {
     TEST_ASSERT_EQUAL_INT(CORRUPTED_MESSAGE, err);
 }
 
-void test_can_read_status_lines1() {
-    TEST_ASSERT_TRUE(1);
+void test_can_read_request_with_long_url() {
+    int ret;
+    http_req_t msg;
+    http_error_t err;
+
+    ret = __read_request(
+        "GET /very/long/path/2/my/site HTTP/1.4\r\n"
+        "status: val\r\n"
+        "\r\n"
+        "body body body\r\n"
+        "\r\n",
+        &msg,
+        &err);
+    TEST_ASSERT_EQUAL_INT(40,                             ret);
+    TEST_ASSERT_EQUAL_INT(GET,                            msg.method);
+    TEST_ASSERT_EQUAL_STRING("/very/long/path/2/my/site", msg.url);
+    TEST_ASSERT_EQUAL_STRING("HTTP/1.4",                  msg.version);
 }
 
 
+void test_can_read_one_status_line() {
+    int ret;
+    http_header_t* headers;
+    unsigned num_headers;
+    http_error_t err;
+    
+    ret = __read_headers(
+        "status: val\r\n"
+        "\r\n"
+        "body body body\r\n",
+        &headers,
+        &num_headers,
+        &err);
+    TEST_ASSERT_EQUAL_INT(15,          ret);
+    TEST_ASSERT_EQUAL_UINT(1,          num_headers);
+    TEST_ASSERT_EQUAL_STRING("status", headers[0].field_name);
+    TEST_ASSERT_EQUAL_STRING("val",    headers[0].value);
+}
+
+void test_can_read_several_status_lines() {
+    int ret;
+    http_header_t* headers;
+    unsigned num_headers;
+    http_error_t err;
+
+    ret = __read_headers(
+        "Set-Cookie: 123\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "bodyyyyyy",
+        &headers,
+        &num_headers,
+        &err);
+    TEST_ASSERT_EQUAL_INT(38,              ret);
+    TEST_ASSERT_EQUAL_UINT(2,              num_headers);
+    TEST_ASSERT_EQUAL_STRING("Set-Cookie", headers[0].field_name);
+    TEST_ASSERT_EQUAL_STRING("123",        headers[0].value);
+    TEST_ASSERT_EQUAL_STRING("Connection", headers[1].field_name);
+    TEST_ASSERT_EQUAL_STRING("close",      headers[1].value);
+    free(headers);
+}
+
+void test_detects_error_on_ill_formed_status_line() {
+    int ret;
+    http_header_t* header;
+    unsigned num_headers;
+    http_error_t err;
+
+    ret = __read_headers(
+        "asdfalsdjfñ\r\n"
+        "Set-Cookie: 123\r\n"
+        "\r\n"
+        "bodyyyy",
+        &header,
+        &num_headers,
+        &err);
+    TEST_ASSERT_LESS_THAN_INT(0,             ret);
+    TEST_ASSERT_EQUAL_INT(CORRUPTED_MESSAGE, err);
+}
+
+void test_can_read_three_status_lines() {
+    int ret;
+    http_header_t* header;
+    unsigned num_headers;
+    http_error_t err;
+
+    ret = __read_headers(
+        "a: 1\r\n"
+        "b: 2\r\n"
+        "c: 3\r\n"
+        "\r\n",
+        &header,
+        &num_headers,
+        &err);
+    TEST_ASSERT_EQUAL_INT(20,     ret);
+    TEST_ASSERT_EQUAL_UINT(3,     num_headers);
+    TEST_ASSERT_EQUAL_STRING("a", header[0].field_name);
+    TEST_ASSERT_EQUAL_STRING("1", header[0].value);
+    TEST_ASSERT_EQUAL_STRING("b", header[1].field_name);
+    TEST_ASSERT_EQUAL_STRING("2", header[1].value);
+    TEST_ASSERT_EQUAL_STRING("c", header[2].field_name);
+    TEST_ASSERT_EQUAL_STRING("3", header[2].value);
+}
+
+void test_can_read_complete_request() {
+    int fd;
+    http_req_t* msg;
+    http_error_t err;
+
+    fd = open("resources/test/http/http1.txt", O_RDONLY);
+    if (fd == -1) {
+        TEST_FAIL();
+    }
+
+    msg = http_read_request(fd, &err);
+    TEST_ASSERT_NOT_NULL(msg);
+    TEST_ASSERT_EQUAL_INT(GET,               msg->method);
+    TEST_ASSERT_EQUAL_STRING("/",            msg->url);
+    TEST_ASSERT_EQUAL_STRING("HTTP/1.1",     msg->version);
+    TEST_ASSERT_EQUAL_UINT(1,                msg->num_headers);
+    TEST_ASSERT_EQUAL_STRING("Host",         msg->headers[0].field_name);
+    TEST_ASSERT_EQUAL_STRING("www.sstt.org", msg->headers[0].value);
+    
+    http_drop_request(msg);
+
+    close(fd);
+}
+
 int main() {
     UNITY_BEGIN();
-    RUN_TEST(test_can_read_request1);
-    RUN_TEST(test_can_read_request2);
-    RUN_TEST(test_can_read_request3);
+    RUN_TEST(test_can_read_request_with_empty_url);
+    RUN_TEST(test_can_read_request_with_file_in_url);
+    RUN_TEST(test_fails_to_read_ill_formed_request);
+    RUN_TEST(test_can_read_request_with_long_url);
+    RUN_TEST(test_can_read_one_status_line);
+    RUN_TEST(test_can_read_several_status_lines);
+    RUN_TEST(test_detects_error_on_ill_formed_status_line);
+    RUN_TEST(test_can_read_three_status_lines);
+    RUN_TEST(test_can_read_complete_request);
     return UNITY_END();
 }
